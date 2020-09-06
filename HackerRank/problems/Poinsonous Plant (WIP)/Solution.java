19005180f4428a3f0223da0e132bd09044949979
A.L <32211153+iluvjava@users.noreply.github.com>
2020-09-05 18:30:20 -0700

    l

--- HackerRank/problems/Poinsonous Plant (WIP)/Solution.java
+++ HackerRank/problems/Poinsonous Plant (WIP)/Solution.java
@@ -1,13 +1,27 @@
 import java.util.*;
-
+import java.lang.System;
 class Solution {
 
     public static void main(String[] args)
     {
-        
+        System.out.println("Has been running");
+        int arr[] = new int[]{1, 2, 3, 4, 2, 3, 4};
+        System.out.println("Output:");
+        System.out.println(solution(arr));
     }
 
     public static int solution(int[] arr)
+    {
+        LinkedList<LinkedList<Integer>> Groups = GroupsIt(arr);
+        while (Groups.size() >= 0)
+        {
+            solution_cancellation(Groups);
+
+        }
+        return 0; 
+    }
+
+    public static LinkedList<LinkedList<Integer>> GroupsIt(int[] arr)
     {
         LinkedList<LinkedList<Integer>> Groups = new LinkedList<>();
         {
@@ -29,27 +43,40 @@ class Solution {
                     Groups.getLast().add(arr[I]);
                 }
             }
+            System.out.println("Constructed Group: ");
+            for (LinkedList<Integer> L : Groups) {
+                System.out.println(L);
+            }
         }
-        while (Groups.size() >= 0)
-        {
-
-        }
-        return 0; 
+        return Groups;
     }
 
     public static boolean solution_cancellation(LinkedList<LinkedList<Integer>> arrs)
     {
-        ListIterator<LinkedList<Integer>> Litr = arrs.listIterator(1);
-        while(Litr.hasNext())
+        ListIterator<LinkedList<Integer>> Litr = arrs.listIterator(0);
         {
-            
+            LinkedList<Integer> Pre = Litr.next();  // Skip the first list. 
+            while(Litr.hasNext())
+            {
+                LinkedList<Integer> Cur = Litr.next();
+                Cur.removeFirst();
+                if (Cur.size() == 0)
+                {
+                    Litr.remove();
+                }
+            }
         }
         return true;
     }
 
     public static boolean solution_merge(LinkedList<LinkedList<Integer>> arrs)
     {
-        
+        Iterator<LinkedList<Integer>> Itr = arrs.iterator();
+        LinkedList<Integer> Pre = Itr.next();
+        {
+            LinkedList<Integer> Cur= Itr.next();
+            
+        }
         return true;
     }
 }
