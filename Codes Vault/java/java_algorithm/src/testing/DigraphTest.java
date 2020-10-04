3ff1f8d008b312cd381456d61f07f32d3f90a964
Alto Legato <victor1301166040@gmail.com>
2020-10-03 19:56:32 -0700

    d

---
+++ Codes Vault/java/java_algorithm/src/testing/DigraphTest.java
@@ -0,0 +1,22 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DigraphTest {
+
+    @org.junit.jupiter.api.Test
+    void addVertex() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addEdges() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasEdge() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNeighbours() {
+    }
+}
